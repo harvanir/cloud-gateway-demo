9921758677a2c3b548bbcbabeaee9c285d127f50
harvanir <harvanjns@gmail.com>
2019-09-21 21:54:37 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Getting Started
+
+### Libraries & Tech
+* Gradle 5.6
+* Spring Boot 2.1.8
+* Spring Cloud Gateway
+
+### Gradle command
+#### Clean
+```shell script
+./gradlew clean
+```
+#### Build
+```shell script
+./gradlew build
+```
+### Run Go service
+```shell script
+./binary/hello-go-mac-os
+```
---
+++ src/main/java/org/harvanir/cloud/cloudgateway/CloudGatewayDemoApplication.java
@@ -0,0 +1,13 @@
+package org.harvanir.cloud.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** @author Harvan Irsyadi */
+@SpringBootApplication
+public class CloudGatewayDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CloudGatewayDemoApplication.class, args);
+  }
+}
